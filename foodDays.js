0152eab19a4546458e1b65667a7f2b7b441bc1ca
Niklas Petersson <np222dp@student.lnu.se>
2024-04-18 15:40:04 +0200

    x

--- foodDays.js
+++ foodDays.js
@@ -1,5 +1,5 @@
 const foodDays = [
-    { date: '2024-04-18', image: 'pannkakor.jpg', title: 'Pannkaksdagen', description: 'Idag är det pannkaksdagen! Passa på att njuta av pannkakor med sylt och grädde.' },
+    { date: '2024-04-18', image: 'images/Food-Days/pannkakor.jpg', title: 'Pannkaksdagen', description: 'Idag är det pannkaksdagen! Passa på att njuta av pannkakor med sylt och grädde.' },
     { date: '2024-10-06', image: 'nudlar.jpg', title: 'Nudeldagen', description: 'Fira nudeldagen med dina favoritnudlar!' }
     // Lägg till fler matdagar här
 ];
