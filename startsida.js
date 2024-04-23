090bcfd7e5c6a22db904dabdb682dff3aed14779
Niklas Petersson <np222dp@student.lnu.se>
2024-04-23 13:29:33 +0200

    x

--- startsida.js
+++ startsida.js
@@ -1,19 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     var videos = [
+        "images/videos/videos-startsida/dagsfornymat.mp4",
         "images/videos/videos-startsida/musslorkokar.mp4",
         "images/videos/videos-startsida/pastapincett.mp4",
         "images/videos/videos-startsida/pizza1.mp4",
         "images/videos/videos-startsida/runtmatbord1.mp4",
         "images/videos/videos-startsida/pizzamozzarella.mp4"
-        // Lägg till alla videor upp till 25
     ];
 
     var videoElement = document.getElementById('randomVideo');
     videoElement.innerHTML = ''; // Tömmer tidigare satta sources
 
     var randomIndex = Math.floor(Math.random() * videos.length);
-    var sourceElement = document.createElement('source');
+    console.log("Random index: " + randomIndex); // Loggar det slumpmässiga indexet
 
+    var sourceElement = document.createElement('source');
     sourceElement.src = videos[randomIndex] + '?_=' + new Date().getTime(); // Lägg till en unik tidstämpel för varje request
     sourceElement.type = 'video/mp4';
 
