c78ae4e7a0d9922f80bd0ccdb2ff6a58612959ef
Niklas Petersson <np222dp@student.lnu.se>
2024-04-23 15:20:33 +0200

    artikelsystem implementerat

--- startsida.js
+++ startsida.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     var videos = [
-        "images/videos/videos-startsida/dagsfornymat.mp4",
+        "images/videos/videos-startsida/runtmatbord1.mp4",
         "images/videos/videos-startsida/musslorkokar.mp4",
         "images/videos/videos-startsida/pastapincett.mp4",
         "images/videos/videos-startsida/pizza1.mp4",
-        "images/videos/videos-startsida/runtmatbord1.mp4",
         "images/videos/videos-startsida/pizzamozzarella.mp4"
     ];
 
