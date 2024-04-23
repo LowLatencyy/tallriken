cc73e8a6f4a7d3d06fc367a800ac079cafd40f4b
Niklas Petersson <np222dp@student.lnu.se>
2024-04-23 12:15:48 +0200

    testar ny js kod för att spela upp videos på startsida

--- startsida.js
+++ startsida.js
@@ -5,16 +5,19 @@ document.addEventListener('DOMContentLoaded', function () {
         "images/videos/videos-startsida/pizza1.mp4",
         "images/videos/videos-startsida/runtmatbord1.mp4",
         "images/videos/videos-startsida/pizzamozzarella.mp4"
+        // Lägg till alla videor upp till 25
     ];
 
     var videoElement = document.getElementById('randomVideo');
     var randomIndex = Math.floor(Math.random() * videos.length);
-    var sourceElement = document.createElement('source');
-    var cacheBuster = "?v=" + new Date().getTime();
+    var cacheBuster = "?v=" + new Date().getTime(); // Skapar en unik query string för att förhindra cache
 
+    // Skapar ett nytt source-element och tillämpar den nya source med cacheBuster
+    videoElement.innerHTML = ''; // Tömmer tidigare satta sources
+    var sourceElement = document.createElement('source');
     sourceElement.setAttribute('src', videos[randomIndex] + cacheBuster);
     sourceElement.setAttribute('type', 'video/mp4');
     videoElement.appendChild(sourceElement);
 
-    videoElement.load();
+    videoElement.load(); // Ladda om videospelaren med den nya källan
 });
