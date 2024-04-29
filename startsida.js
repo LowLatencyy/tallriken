598b3908721ab97a30a944f9d752e52eea219809
Niklas Petersson <np222dp@student.lnu.se>
2024-04-29 17:31:17 +0200

    x

--- startsida.js
+++ startsida.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var videos = [
-        "images/videos/videos-startsida/runtmatbord2.mp4.mp4",
-        "images/videos/videos-startsida/runtmatbord2.mp4.mp4"
+        "images/videos/videos-startsida/runtmatbord2.mp4",
+        "images/videos/videos-startsida/runtmatbord2.mp4"
        // "images/videos/videos-startsida/pastapincett.mp4",
         //"images/videos/videos-startsida/pizza1.mp4",
         //"images/videos/videos-startsida/pizzamozzarella.mp4"
