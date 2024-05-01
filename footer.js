3e4070ff672440efc588c25778f4d34f3a6008b2
Niklas Petersson <np222dp@student.lnu.se>
2024-05-01 18:28:17 +0200

    test för att försöka lösa förra push misslyckandet som resulterade i ett stort meddelande på skärmen finns ingen github site

--- footer.js
+++ footer.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const footerContainer = document.getElementById('footer-container');
     if (footerContainer) {
-        fetch('/footer.html')  // Använd absolut sökväg här
+        fetch('footer.html')  // Använd absolut sökväg här
             .then(response => response.text())
             .then(html => footerContainer.innerHTML = html)
             .catch(error => console.error('Failed to load footer:', error));
--- loadNavbar.js
+++ loadNavbar.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const navbarContainer = document.getElementById('navbar-container');
     if (navbarContainer) {
-        fetch('/navbar.html')
+        fetch('navbar.html')
             .then(response => response.text())
             .then(html => navbarContainer.innerHTML = html)
             .catch(error => console.error('Failed to load navbar:', error));
