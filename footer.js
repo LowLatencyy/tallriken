4133c1b33d0a2d8d0bebdeca50146f9fe610eb56
Niklas Petersson <np222dp@student.lnu.se>
2025-01-28 15:54:36 +0100

    ny footer implemetnerad över alla sidor

--- footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const footerContainer = document.getElementById('footer-container');
-    if (footerContainer) {
-        fetch('footer.html')  // Använd absolut sökväg här
-            .then(response => response.text())
-            .then(html => footerContainer.innerHTML = html)
-            .catch(error => console.error('Failed to load footer:', error));
-    }
-});
